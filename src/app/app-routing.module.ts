39472776386f86a473956171df3cf87b3e8366a1
minhdat2708 <minhdat01.dev@gmail.com>
2022-09-23 18:08:56 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,36 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {HomeComponent} from "./home/home.component";
+import {LoginComponent} from "./admin/login/login.component";
+import {AdminComponent} from "./admin/admin.component";
+import {DashboardComponent} from "./admin/dashboard/dashboard.component";
+import {EmployeeComponent} from "./admin/employee/employee.component";
+import {OrganizationComponent} from "./admin/organization/organization.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent,
+    children: [
+      {path: 'login', component: LoginComponent},
+      {
+        path: 'admin',
+        component: AdminComponent,
+        children: [
+          {path: '', pathMatch: "full", redirectTo: 'dashboard'},
+          {path: 'dashboard', component: DashboardComponent},
+          {path: 'employee', component: EmployeeComponent},
+          {path: 'organization', component: OrganizationComponent},
+        ]
+      }
+    ]
+  }
+];
 
 @NgModule({
+  declarations: [],
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdminComponent } from './admin/admin.component';
+import { EmployeeComponent } from './admin/employee/employee.component';
+import { OrganizationComponent } from './admin/organization/organization.component';
+import { LoginComponent } from './admin/login/login.component';
+import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './admin/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminComponent,
+    EmployeeComponent,
+    OrganizationComponent,
+    LoginComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
